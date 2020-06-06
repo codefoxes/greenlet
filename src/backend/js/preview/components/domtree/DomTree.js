2e62f9c0112f558cc31beb3806709c569e006875
Karthik Bhat <karthik.ax.ax@gmail.com>
2020-06-06 16:49:52 +0530

    Domtree selector

---
+++ src/backend/js/preview/components/domtree/DomTree.js
@@ -0,0 +1,132 @@
+import { getSelector } from './DomTreeHandler'
+import { DomTreeStore, useStore } from './DomTreeStore'
+import styles from './DomTree.scss'
+import SelectSearch from 'react-select-search'
+import SelectStyles from './Select.scss'
+
+function DomTree() {
+    const { currentTarget, showDomTree } = useStore( DomTreeStore )
+
+	return (
+		<div id="cw-domtree">
+            { showDomTree ? DomTreeElement( getSelector( currentTarget ) ) : 'Click any element to show DOM element tree here' }
+            <style type="text/css">{ styles }</style>
+            <style type="text/css">{ SelectStyles }</style>
+		</div>
+	)
+}
+
+function DomTreeElement(domTree) {
+    return (
+        <ul className="cw-domtree-list">
+            { 
+                domTree.map( ( element, i ) => (
+                    //onClick={ () => doNothingForNow( element ) }
+                    <li key={ `${element.tag}-${i}` } className="cw-domtree-elements">
+                        { element.tag }
+                        { ( element.id || ( element.class && element.class.length > 0 ) ) ?
+                            <div className="cw-node-attributes">
+                                {/*
+                                <DomIdAttribute id={ element.id } />
+                                <DomClassAttribute classes={ element.class } />
+                                 <SelectSearch options={[
+                                    {
+                                        name: 'Select Id',
+                                        type: 'group',
+                                        items: [{
+                                            value: 'hamburger',
+                                            name: 'Hamburger',
+                                        }, {
+                                            value: 'pizza',
+                                            name: 'Pizza',
+                                        }]
+                                    },
+                                    {
+                                        name: 'Select Classes',
+                                        type: 'group',
+                                        items: [{
+                                            value: 'soft',
+                                            name: 'Soft drink',
+                                        }, {
+                                            value: 'beer',
+                                            name: 'Beer',
+                                        }]
+                                    }
+                                ]}
+                                multiple
+                                /> */}
+                                <DomAttributeSelectSearch element={ element } />
+                            </div>
+                        : null
+                        }
+                    </li>
+                ) ) 
+            }
+        </ul> 
+    )
+    //return "Constructed DOM Tree"
+}
+function DomAttributeSelectSearch( { element } ) {
+    const selectOptions = []
+    if ( element.id ) {
+        selectOptions.push( {
+            name: 'Select Id',
+            type: 'group',
+            items: [ {
+                value: element.id,
+                name: element.id,
+            } ]
+        })
+    }
+
+    if ( element.class && element.class.length > 0 ) {
+        const classOptions = []
+        element.class.map( ( cls, i ) => (
+            classOptions.push( {
+                value: cls,
+                name: cls,
+            } )
+        ) )
+
+        selectOptions.push({
+            name: 'Select Class',
+            type: 'group',
+            items: classOptions
+        })
+    }
+
+    return (
+        <SelectSearch options={ selectOptions } multiple />
+    )
+}
+
+function DomIdAttribute( { id } ) {
+    if ( !id ) {
+        return null;
+    }
+
+    return (
+        <>
+            <h3 className="cw-selection-title">Select Id</h3>
+            <span> { id } </span>
+        </>
+    )
+}
+function DomClassAttribute( { classes } ) {
+    if ( !classes || classes.length === 0 ) {
+        return null;
+    }
+   
+    return (
+        <>
+            <h3 className="cw-selection-title">Select class</h3>
+            <ul>
+                { classes.map( ( cls, i ) => (
+                <li key={ i }> { cls } </li>
+                ) ) }
+            </ul>
+        </>
+    )
+}
+
+export default DomTree
---
+++ src/backend/js/preview/components/domtree/DomTreeStore.js
@@ -0,0 +1,18 @@
+import { Store, useStore } from '../../../common/Store'
+
+const initialState = {
+	currentTarget: '',
+    showDomTree: false
+}
+
+class DomTreeClass extends Store {
+    showDomTree( data ) {
+        this.set( () => ( { currentTarget: data.currentTarget } ) )
+        this.set( () => ( { showDomTree: true } ) )
+    }
+}
+
+
+const DomTreeStore = new DomTreeClass( initialState )
+
+export { DomTreeStore, useStore }
